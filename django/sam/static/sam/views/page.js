ae9e44beedb8e83f55e47dced05721571a79e4ff
Denis Gladkikh <denis@gladkikh.email>
2014-06-11 15:59:05 -0600

    Made simple object models from d3 example

--- django/sam/static/sam/views/page.js
+++ django/sam/static/sam/views/page.js
@@ -17,6 +17,125 @@ define(
   var PageModel = require('sam/models/page');
   var d3 = require('d3');
 
+  var dragBehavior = d3.behavior.drag().origin(Object).on("drag", function dragMove(dragged) {
+    if (dragged.move) {
+      dragged.move(d3.event.dx, d3.event.dy);
+    }
+  });
+
+  var lineInterpolate = d3.svg.line().interpolate("basis");
+
+  var Nodes = Backbone.Model.extend({
+    defaults: {
+      dsvg: null,
+      jsvg: null
+    },
+
+    initialize: function() {
+      this._nodes = {};
+      this._links = {};
+    },
+
+    draw: function() {
+      var dsvg = this.get('dsvg');
+      this._gCircles = dsvg.append("g").attr("class", "circles");
+      this._gLines = dsvg.append("g").attr("class", "lines");
+    },
+
+    addNode: function(id, settings){
+      var node = new Node(_.extend(settings, {
+        gCircle: this._gCircles.append("circle"),
+        id: id,
+        pageWidth: this.get('jsvg').width(),
+        pageHeight: this.get('jsvg').height()
+      }));
+      this._nodes[id] = node;
+      this.listenTo(node, 'change:cx change:cy', this._updateLinks);
+    },
+
+    updateLink: function(sourceId, targetId, settings) {
+      var id = sourceId + "@" + targetId;
+      var link = new Link({
+        id: id,
+        source: this._nodes[sourceId],
+        target: this._nodes[targetId],
+        gPath: this._gLines.append('path')
+      });
+      this._links[id] = link;
+    }
+  });
+
+  var Link = Backbone.Model.extend({
+    defaults: {
+      id: null,
+      source: null,
+      target: null,
+      gPath: null
+    },
+
+    initialize: function() {
+      this.get('gPath')
+        .datum(this)
+        .attr("class", "link_line")
+        .attr("id", this.get('id'));
+
+      this.listenTo(this.get('source'), 'change:cx change:cy', this._drawCurve);
+      this.listenTo(this.get('target'), 'change:cx change:cy', this._drawCurve);
+
+      this._drawCurve();
+    },
+
+    _drawCurve: function() {
+      var source = this.get('source');
+      var target = this.get('target');
+
+      var points = [
+        [source.get('cx'), source.get('cy')],
+        [target.get('cx'), target.get('cy')]
+      ];
+
+      this.get('gPath').attr('d', lineInterpolate(points));
+    }
+  });
+
+  var Node = Backbone.Model.extend({
+    defaults: {
+      radius: 30,
+      cx: 0,
+      cy: 0,
+      gCircle: null
+    },
+
+    initialize: function() {
+      this.on('change:cx change:cy', this._redraw);
+
+      var gCircle = this.get('gCircle');
+      gCircle
+        .datum(this)
+        .attr("class", "circle")
+        .attr("id", this.get('id'))
+        .attr("r", this.get('radius'))
+        .attr("cx", this.get('cx'))
+        .attr("cy", this.get('cy'))
+        .call(dragBehavior);
+    },
+
+    move: function(dx, dy) {
+      var x = this.get('cx');
+      var y = this.get('cy');
+      this.set({
+        cx: Math.max(this.get('radius'), Math.min(this.get('pageWidth') - this.get('radius'), + x + dx)),
+        cy: Math.max(this.get('radius'), Math.min(this.get('pageHeight') - this.get('radius'), + y + dy))
+      });
+    },
+
+    _redraw: function() {
+      this.get('gCircle')
+        .attr("cx", this.get('cx'))
+        .attr("cy", this.get('cy'));
+    }
+  });
+
   var PageView = Backbone.View.extend({
     el: '#content',
 
@@ -28,7 +147,8 @@ define(
     },
 
     render: function() {
-      this._svg = $('svg');
+      this._dsvg = d3.select('svg');
+      this._jsvg = $('svg');
 
       this._searchManager = mvc.Components.get('appSearchManager');
       this._searchBarView = mvc.Components.get('appSearchBar');
@@ -39,6 +159,11 @@ define(
               latest_time: 'rt'
           });
       }, this), 500);
+
+      this._nodes = new Nodes({
+        dsvg: this._dsvg,
+        jsvg: this._jsvg
+      });
 
       return this;
     },
@@ -100,15 +225,14 @@ define(
     },
 
     _draw: function() {
-      this._svg.empty();
+      this._jsvg.empty();
 
-      this._pageWidth = this._svg.width();
-      this._pageHeight = this._svg.height();
-      this._circleRadius = 30; /* radius of circles */
-      this._offsetScale = 0.1; /* percentage of line line to offset curves */
-      this._d3LineBasis = d3.svg.line().interpolate("basis");
-      
-      var d3color = d3.interpolateRgb("#BAE4B3", "#006D2C"); /* color range for flow lines */
+      this._nodes.draw();
+
+      this._pageWidth = this._jsvg.width();
+      this._pageHeight = this._jsvg.height();
+
+
 
       //A LIST OF LINKS BETWEEN CIRCLES
       var links = [
@@ -146,97 +270,21 @@ define(
           strength: Math.round(Math.random() * 10)}
       ];
 
-      function createDefs(defs) {
-          var dropShadowFilter = defs.append('svg:filter').attr('id', 'dropShadow');
-          dropShadowFilter.append('svg:feGaussianBlur').attr('in', 'SourceAlpha').attr('stdDeviation', 1);
-          dropShadowFilter.append('svg:feOffset').attr('dx', 0).attr('dy', 1).attr('result', 'offsetblur');
-          var feMerge = dropShadowFilter.append('svg:feMerge');
-          feMerge.append('svg:feMergeNode');
-          feMerge.append('svg:feMergeNode').attr('in', "SourceGraphic");
-      }
-
-      var drag = d3.behavior.drag().origin(Object).on("drag", _.bind(function() {
-          this._dragmove(this);
-      }, this));
-
       //RANDOMLY GENERATE COORDINATES FOR CIRCLES
-      var numCircles = 6; /* number of circles - you must update link source/target values to match changes in the number of circles */
-      var circles = d3.range(numCircles).map(_.bind(function(i, d) {
-          return [Math.round(50 + (i / numCircles) * (this._pageWidth - 50)), Math.round(30 + Math.random() * (this._pageHeight - 80))];
+      var numCircles = 6;
+      d3.range(numCircles).map(_.bind(function(i) {
+        this._nodes.addNode(
+          i, 
+          {
+            cx: Math.round(50 + (i / numCircles) * (this._pageWidth - 50)), 
+            cy: Math.round(30 + Math.random() * (this._pageHeight - 80))
+          }
+        );
       }, this));
 
-      //GLOBAL STRENGTH SCALE
-      this._strength_scale = d3.scale.linear().range([2, 10]) /* thickness range for flow lines */
-      .domain([0, d3.max(links, function(d) {
-          return d.strength;
-      })]);
-
-      var color_scale = d3.scale.linear().range([0, 1]).domain([0, d3.max(links, function(d) {
-          return d.strength;
-      })]);
-
-      var svg = d3.select("svg");
-
-      var g_lines = svg.append("g").attr("class", "lines");
-      var g_circles = svg.append("g").attr("class", "circles");
-
-      //SHADOW DEFINITION
-      createDefs(svg.append('svg:defs'));
-
-      $.each(circles, _.bind(function(i, d) {
-          g_circles.append("circle").attr('filter', 'url(#dropShadow)').attr("class", "circle").attr("id", "circle" + i).attr("r", this._circleRadius).attr("cx", d[0]).attr("cy", d[1]).call(drag);
+      links.forEach(_.bind(function(line) {
+        this._nodes.updateLink(line.source, line.target, line);
       }, this));
-
-      g_lines.selectAll(".link_line").data(links).enter().append("path").attr("class", "link_line").attr("fill", _.bind(function(d) {
-          return d3color(color_scale(d.strength));
-      }, this)).attr("id", _.bind(function(i, d) {
-          return "link_line" + d;
-      }, this)).attr("d", _.bind(function(d) {
-          return this._drawCurve(d);
-      }, this));
-    },
-
-    _dragmove: function(dragged) {
-      var x = d3.select(dragged).attr("cx");
-      var y = d3.select(dragged).attr("cy");
-      d3.select(dragged).attr("cx", Math.max(this._circleRadius, Math.min(this._pageWidth - this._circleRadius, +x + d3.event.dx))).attr("cy", Math.max(this._circleRadius, Math.min(this._pageHeight - this._circleRadius, +y + d3.event.dy)));
-      $.each(links, _.bind(function(i, link) {
-        if (link.source == dragged.id.match(/\d+/)[0] || link.target == dragged.id.match(/\d+/)[0]) {
-          d3.select('#link_line' + i).attr("d", _.bind(function(d) {
-            return this._drawCurve(d);
-          }, this));
-        }
-      }, this));
-    },
-
-    _drawCurve: function(d) {
-      var slope = Math.atan2((+d3.select('#circle' + d.target).attr("cy") - d3.select('#circle' + d.source).attr("cy")), (+d3.select('#circle' + d.target).attr("cx") - d3.select('#circle' + d.source).attr("cx")));
-      var slopePlus90 = Math.atan2((+d3.select('#circle' + d.target).attr("cy") - d3.select('#circle' + d.source).attr("cy")), (+d3.select('#circle' + d.target).attr("cx") - d3.select('#circle' + d.source).attr("cx"))) + (Math.PI/2);
-
-      var sourceX = +d3.select('#circle' + d.source).attr("cx");
-      var sourceY = +d3.select('#circle' + d.source).attr("cy");
-      var targetX = +d3.select('#circle' + d.target).attr("cx");
-      var targetY = +d3.select('#circle' + d.target).attr("cy");
-
-      var halfX = (sourceX + targetX)/2;
-      var halfY = (sourceY + targetY)/2;
-
-      var lineLength = Math.sqrt(Math.pow(targetX - sourceX, 2) + Math.pow(targetY - sourceY, 2));
-
-      var MP1X = halfX + (this._offsetScale * lineLength + this._strength_scale(d.strength)/2) * Math.cos(slopePlus90);
-      var MP1Y = halfY + (this._offsetScale * lineLength + this._strength_scale(d.strength)/2) * Math.sin(slopePlus90);
-      var MP2X = halfX + (this._offsetScale * lineLength - this._strength_scale(d.strength)/2) * Math.cos(slopePlus90);
-      var MP2Y = halfY + (this._offsetScale * lineLength - this._strength_scale(d.strength)/2) * Math.sin(slopePlus90);
-
-      var points = [];
-      points.push([(sourceX - this._strength_scale(d.strength) * Math.cos(slopePlus90)),(sourceY - this._strength_scale(d.strength) * Math.sin(slopePlus90))]);
-      points.push([MP2X,MP2Y]);
-      points.push(([(targetX  + this._circleRadius * Math.cos(slope)), (targetY + this._circleRadius * Math.sin(slope))]));
-      points.push(([(targetX  + this._circleRadius * Math.cos(slope)), (targetY + this._circleRadius * Math.sin(slope))]));
-      points.push([MP1X, MP1Y]);
-      points.push([(sourceX + this._strength_scale(d.strength) * Math.cos(slopePlus90)),(sourceY + this._strength_scale(d.strength) * Math.sin(slopePlus90))]);
-
-      return this._d3LineBasis(points) + "Z";
     }
   });
 
